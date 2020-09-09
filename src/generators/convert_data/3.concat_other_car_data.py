fa8a466c378e36542ceedac5309296c6a5cead29
changshuangzhao <changshuangzhao@126.com>
2020-09-09 21:15:24 +0800

    更新

--- src/generators/convert_data/3.concat_other_car_data.py
+++ src/generators/convert_data/3.concat_other_car_data.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
-root = '/Users/yanyan/data/object_detection/neg_hard'
+root = '/Users/yanyan/data/object_detection/neg_voc'
 
 image_names = os.listdir(root)
 for image_name in image_names:
     if '.DS_Store' in image_name:
         continue
     with open('train.csv', 'a') as f:
-        f.write('object_detection/neg_hard/' + image_name + ',' * 6 + '\n')
+        f.write('object_detection/neg_voc/' + image_name + ',' * 6 + '\n')
 
